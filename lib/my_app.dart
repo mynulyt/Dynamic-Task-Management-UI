731953aa63e4cb13cbfa058052d6b2e2e7a44ee1
mynulyt <mynulalam27@gmail.com>
2025-09-10 16:15:58 +0100

    Dynamic Task Manager

---
+++ lib/my_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskManager extends StatelessWidget {
+  const TaskManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

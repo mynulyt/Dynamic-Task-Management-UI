a669dfb776e7d5ba99d1ea3ea8859cd44cd53fe7
mynulyt <mynulalam27@gmail.com>
2025-09-10 17:06:07 +0100

    asset path set up

---
+++ lib/UI/Utils/asset_path.dart
@@ -0,0 +1,5 @@
+class AssetPath {
+  static const String _assetPat = 'assets/';
+  static const String logoSvg = '$_assetPat/logo.svg';
+  static const String bgSvg = '$_assetPat/background.svg';
+}

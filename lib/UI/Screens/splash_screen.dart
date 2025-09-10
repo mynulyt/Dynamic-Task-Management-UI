29ee1426fdeb3bf7f89cc3a0d59b8cabde635868
mynulyt <mynulalam27@gmail.com>
2025-09-10 17:16:34 +0100

    splash screen

--- lib/UI/Screens/splash_screen.dart
+++ lib/UI/Screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:task_manager/UI/Screens/login_screen.dart';
+import 'package:task_manager/UI/Utils/asset_path.dart';
 import 'package:task_manager/UI/widgets/backgorund_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -26,6 +28,10 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: BackgorundScreen());
+    return Scaffold(
+      body: BackgorundScreen(
+        child: Center(child: SvgPicture.asset(AssetPath.logoSvg, height: 40)),
+      ),
+    );
   }
 }
--- lib/UI/widgets/backgorund_screen.dart
+++ lib/UI/widgets/backgorund_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/UI/Utils/asset_path.dart';
 
 class BackgorundScreen extends StatelessWidget {
-  const BackgorundScreen({super.key});
+  const BackgorundScreen({super.key, required this.child});
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        SvgPicture.asset(
+          AssetPath.bgSvg,
+          height: double.maxFinite,
+          width: double.maxFinite,
+          fit: BoxFit.cover,
+        ),
+        SafeArea(child: child),
+      ],
+    );
   }
 }

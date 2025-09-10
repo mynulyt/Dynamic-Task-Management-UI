a67d57fded7b63787030bfbb8f06a17bdc7daecb
mynulyt <mynulalam27@gmail.com>
2025-09-10 17:37:53 +0100

    widget file

---
+++ lib/UI/widgets/task_count_by_status_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskCountByStatusCard extends StatelessWidget {
+  const TaskCountByStatusCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/UI/widgets/tm_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TmAppBar extends StatelessWidget {
+  const TmAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

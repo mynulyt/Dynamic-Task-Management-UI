8d449092f1a84376715e10eb6e0acbf44dd69b34
mynulyt <mynulalam27@gmail.com>
2025-09-11 16:42:56 +0100

    TmAppBar

--- lib/UI/widgets/tm_app_bar.dart
+++ lib/UI/widgets/tm_app_bar.dart
@@ -5,6 +5,14 @@ class TmAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      tileColor: Colors.white,
+      title: Text("Title will be here"),
+      subtitle: Column(children: [
+          
+        ],
+      ),
+    );
   }
 }

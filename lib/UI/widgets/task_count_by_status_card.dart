579248047d273350891e32362ba011c532f1d60f
mynulyt <mynulalam27@gmail.com>
2025-09-11 17:00:00 +0100

    task count by status card

--- lib/UI/widgets/task_count_by_status_card.dart
+++ lib/UI/widgets/task_count_by_status_card.dart
@@ -1,10 +1,31 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class TaskCountByStatusCard extends StatelessWidget {
-  const TaskCountByStatusCard({super.key});
+  const TaskCountByStatusCard({
+    super.key,
+    required this.title,
+    required this.count,
+  });
+
+  final String title;
+  final Int count;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+        child: Column(
+          children: [
+            Text('$count', style: Theme.of(context).textTheme.titleLarge),
+            Text(title, style: TextStyle(color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
   }
 }

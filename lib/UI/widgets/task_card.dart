1bff7cb4f0922710f578e9bcfb78a6b9bca4e703
mynulyt <mynulalam27@gmail.com>
2025-09-11 16:54:23 +0100

    task card

--- lib/UI/widgets/task_card.dart
+++ lib/UI/widgets/task_card.dart
@@ -12,7 +12,38 @@ class TaskCard extends StatelessWidget {
       subtitle: Column(
         spacing: 8,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [],
+        children: [
+          Text("Description of Task"),
+          Text(
+            "Date:10/10/2025",
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+
+          Row(
+            children: [
+              Chip(
+                label: Text("New"),
+
+                backgroundColor: Colors.blue,
+                labelStyle: TextStyle(color: Colors.white),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.grey),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.edit, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

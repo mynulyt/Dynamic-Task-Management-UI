17ec30197dafa284ff39e46e492be19376634dca
mynulyt <mynulalam27@gmail.com>
2025-09-12 18:39:55 +0100

    login screen

--- lib/UI/Screens/login_screen.dart
+++ lib/UI/Screens/login_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/UI/widgets/backgorund_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BackgorundScreen(child: ),
+    );
   }
 }
